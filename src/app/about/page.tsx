5426ceff9e28a5730d61b1e394e1081f827daa5a
sanchitadevi <thesanchitadevi@gmail.com>
2023-09-24 00:52:31 +0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div>
+        About page
+      {/* <NotFound /> */}
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/members/executive/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Executive = () => {
+  return <div>Executive</div>;
+};
+
+export default Executive;
