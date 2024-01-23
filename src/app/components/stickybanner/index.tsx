fbadfaf20ee80416aa2ce69f2752fffddad65630
sanchitadevi <thesanchitadevi@gmail.com>
2024-01-23 19:13:38 +0600

    changes

--- src/app/components/stickybanner/index.tsx
+++ src/app/components/stickybanner/index.tsx
@@ -44,7 +44,7 @@ const StickyBanner: React.FC = () => {
             >
               <circle cx="1" cy="1" r="1" />
             </svg>
-            Registration date from Jan 14 – Jan 23 (Extended 31 Jan)
+            Registration date from Jan 14 – Jan 23
           </p>
           <Link
             href="https://forms.gle/1wAqvCFGHNYHDTN16"
