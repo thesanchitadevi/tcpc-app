88c7214027666af4c1cf6eea66be417c036832e2
sanchitadevi <thesanchitadevi@gmail.com>
2024-02-05 20:09:39 +0600

    events added

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,38 +1,61 @@
-import React from "react";
-import Upcoming from "./upcoming";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import axios from "axios";
+
+interface Events {
+  id: number;
+  image: string;
+  title: string;
+  details: string;
+  //   events: any;
+}
+const fetchData = async (): Promise<Events[]> => {
+  const response = await axios.get("/events/events.json");
+  return response.data as Events[];
+};
 
 const Events: React.FC = () => {
+  const [data, setData] = useState<Events[]>([]);
+
+  useEffect(() => {
+    fetchData().then((data) => setData(data));
+  }, []);
+  //   console.log(data);
+
   return (
     <>
       <div className="px-4 my-20 mx-auto max-w-6xl md:px-0 ">
-        <Upcoming />
         <h2 className="my-20 font-sans text-4xl text-center font-bold md:tracking-wide text-white  leading-none uppercase">
           Featured Events
         </h2>
 
-        <div className="flex flex-col overflow-hidden rounded-md shadow-sm md:flex-row">
-          <Image
-            src="/events/combat_2021.jpeg"
-            alt="Programming Club"
-            className="h-80 "
-            width="500"
-            height="150"
-          />
-          <div className="flex flex-col justify-center flex-1 p-6 ">
-            <h3 className="text-3xl font-bold text-primary">
-              Programming Combat 2021
-            </h3>
-            <p className="my-6 text-gray-200">
-              Programming Combat is one of the biggest and most awaited events
-              organized successfully by Tejgaon College Programming Club. This
-              occasion is for both the students and faculties of Tejgaon College
-              which brings the institutions together with its celebration by
-              students of different disciplines, the honorable Vice-Chancellor
-              of National University, Faculty members, and Administrative
-              officers.
-            </p>
-          </div>
+        <div>
+          {data?.map?.(
+            (s: {
+              id: number;
+              image: string;
+              title: string;
+              details: string;
+            }) => (
+              <div
+                key={s.id}
+                className="flex flex-col overflow-hidden rounded-md shadow-sm md:flex-row"
+              >
+                <Image
+                  src={s.image}
+                  alt="Programming Club"
+                  className="h-80 "
+                  width="500"
+                  height="150"
+                />
+                <div className="flex flex-col justify-center flex-1 p-6 ">
+                  <h3 className="text-3xl font-bold text-primary">{s.title}</h3>
+                  <p className="my-6 text-gray-200">{s.details}</p>
+                </div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </>
