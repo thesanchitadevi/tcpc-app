32dcc226417556a7e9b1e37e25ee5bc2edbb4a04
Sanchita Devi <thesanchitadevi@gmail.com>
2024-10-08 00:55:27 +0600

    changes added

--- src/app/homePage/stats/index.tsx
+++ src/app/homePage/stats/index.tsx
@@ -5,13 +5,13 @@ const Stats: React.FC = () => {
     <div className="px-4 py-16 mx-auto  md:max-w-5xl ">
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
         <div className="text-center md:border-r border-slate-700">
-          <h6 className="text-4xl font-bold  md:text-6xl">150+</h6>
+          <h6 className="text-4xl font-bold  md:text-6xl">20+</h6>
           <p className="text-sm font-medium tracking-widest text-gray-600 uppercase md:text-base">
             CURRENT MEMBERS
           </p>
         </div>
         <div className="text-center md:border-r border-slate-700">
-          <h6 className="text-4xl font-bold md:text-6xl">12</h6>
+          <h6 className="text-4xl font-bold md:text-6xl">7</h6>
           <p className="text-sm font-medium tracking-widest text-gray-600 uppercase md:text-base">
             MENTORS
           </p>
@@ -23,7 +23,7 @@ const Stats: React.FC = () => {
           </p>
         </div>
         <div className="text-center">
-          <h6 className="text-4xl font-bold md:text-6xl">24</h6>
+          <h6 className="text-4xl font-bold md:text-6xl">20</h6>
           <p className="text-sm font-medium tracking-widest text-gray-600 uppercase md:text-base">
             CURRENT EXECUTIVES
           </p>
