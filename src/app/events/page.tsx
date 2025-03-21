dd736a38f60a67bafcb517875442e980dd7c9363
Sanchita Devi <thesanchitadevi@gmail.com>
2025-03-22 02:14:53 +0600

    changes in event

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -32,17 +32,21 @@ const Events: React.FC = () => {
         </h2>
         <div>
           {data?.map?.(
-            (s: {
-              id: number;
-              image: string;
-              date: string;
-              title: string;
-              details: string;
-            }, index) => (
+            (
+              s: {
+                id: number;
+                image: string;
+                date: string;
+                title: string;
+                details: string;
+              },
+              index
+            ) => (
               <div
                 key={s.id}
-                className={`flex flex-col gap-10 lg:mb-16 mb-8 overflow-hidden rounded-md shadow-sm lg:flex-row justify-center items-center lg:items-start md:px-10 px-5 ${index % 2 !== 0 ? "lg:flex-row-reverse" : ""
-                  }`}
+                className={`flex flex-col gap-10 lg:mb-16 mb-8 overflow-hidden rounded-md shadow-sm lg:flex-row justify-center items-center lg:items-start md:px-10 px-5 ${
+                  index % 2 !== 0 ? "lg:flex-row-reverse" : ""
+                }`}
               >
                 <div className="overflow-hidden rounded-lg">
                   <Image
@@ -54,8 +58,12 @@ const Events: React.FC = () => {
                   />
                 </div>
                 <div className="flex flex-col justify-center flex-1">
-                  <h3 className="text-2xl md:text-3xl font-bold text-primary">{s.title}</h3>
-                  <p className="text-sm text-primary pt-2"><span className="text-lg font-bold">Event date:</span> {s.date}</p>
+                  <h3 className="text-2xl md:text-3xl font-bold text-primary">
+                    {s.title}
+                  </h3>
+                  <p className="text-sm text-primary pt-2">
+                    <span className="font-semibold">Event Date:</span> {s.date}
+                  </p>
                   <p className="mt-6 text-gray-200">{s.details}</p>
                 </div>
               </div>
